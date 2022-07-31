e7475914b4df89c5b4b42979e76777d32da55553
jungleyoung <1664234401@qq.com>
2022-07-31 14:19:57 +0800

    随便改改

--- blog-client/src/test/java/com/julex/blogclient/BlogClientApplicationTests.java
+++ blog-client/src/test/java/com/julex/blogclient/BlogClientApplicationTests.java
@@ -1,10 +1,10 @@
-package com.julex.blogapi;
+package com.julex.blogclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlogApiApplicationTests {
+class BlogClientApplicationTests {
 
     @Test
     void contextLoads() {
--- blog-service/src/main/java/com/julex/blogservice/BlogServiceApplication.java
+++ blog-service/src/main/java/com/julex/blogservice/BlogServiceApplication.java
@@ -1,13 +1,13 @@
-package com.julex.blogapi;
+package com.julex.blogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApiApplication {
+public class BlogServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogApiApplication.class, args);
+        SpringApplication.run(BlogServiceApplication.class, args);
     }
 
 }
